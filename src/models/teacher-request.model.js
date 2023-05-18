29801cfb40bc95f7f483613cc0e68e5661c93df7
Nikita Kubarko <n.kubarko@naveksoft.com>
2023-05-18 23:47:01 +0300

    become teacher client api

---
+++ src/models/teacher-request.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Types = mongoose.Schema.Types
+
+const schema = new mongoose.Schema({
+    uuid: {
+        required: true,
+        type: String,
+        unique: true,
+    },
+	user: {
+		type: Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	motivation: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		enum: ['pending', 'approved', 'rejected'],
+		required: true,
+		default: 'pending'
+	}
+})
+
+const model = mongoose.model('TeacherRequest', schema, 'teacherRequests')
+
+module.exports = {
+    Model: model,
+}
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user.controller');
+const { generateUUID } = require('../helpers/common.helper');
 const authenticate = require('../middlewares/authenticate.middleware');
 const UserTrainingProfile = require('../models/user-training-profile.model');
 const User = require('../models/user.model');
+const TeacherRequest = require('../models/teacher-request.model');
 
 const router = new Router();
 
@@ -15,6 +17,118 @@ router.get(
         return res.status(200).json({
             test: true
         })
+    }
+)
+
+router.get(
+    '/become-teacher/:id',
+    async (req, res) => {
+        const { id } = req.params
+
+        try {
+            const request = await TeacherRequest.Model.findOne({
+                user: id
+            }).populate('user')
+
+            return res.status(200).json({
+                message: 'Get teacher perm status',
+                request,
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Error get teacher permission request status.',
+                error,
+            })
+        }
+    }
+)
+
+router.post(
+    '/become-teacher',
+    async (req, res) => {
+        const { userId, motivation } = req.body.request
+
+        try {
+            const user = await User.Model.findOne({
+                _id: userId,
+            })
+
+            if (!user) {
+                return res.status(400).json({
+                    message: 'No user.',
+                })
+            }
+
+            const request = await (new TeacherRequest.Model({
+                uuid: generateUUID(),
+                user: user._id,
+                motivation,
+                status: 'pending',
+            })).save()
+
+            return res.status(200).json({
+                message: 'Request created. Status: pending',
+                request
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Error get teacher permissions.',
+                error,
+            })
+        }
+    }
+)
+
+router.patch(
+    '/become-teacher',
+    async (req, res) => {
+        const { status, userId } = req.body
+
+        try {
+            const request = await TeacherRequest.Model.findOne({
+                user: userId
+            })
+
+            request.status = status
+            const updated = await request.save()
+
+            return res.status(200).json({
+                message: `Request status changed. Status: ${status}`,
+                updated,
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Error update teacher permission status.',
+                error,
+            })
+        }
+    }
+)
+
+router.delete(
+    '/become-teacher/:id',
+    async (req, res) => {
+        const { id } = req.params
+
+        try {
+            await TeacherRequest.Model.deleteOne({
+                user: id
+            })
+
+            return res.status(200).json({
+                message: 'Deleted teacher perms request',
+                request: null,
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Error delete teacher permission request.',
+                error,
+            })
+        }
     }
 )
 
